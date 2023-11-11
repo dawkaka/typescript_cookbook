6966558f695dcf2b392462d9d0f75d93920dfd2c
dawkaka <yousiph77@gmail.com>
2023-11-11 07:53:55 +0000

    mer

---
+++ dist/any_vs_unkown.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * They are both top types with means every value is compatible with them
+ * `any` is compatible with every sub-type except never. You can narrow the set of possible values by assigning a new type
+ *
+ */
+const me = "Hello world";
+const myName = me;
---
+++ src/any_vs_unkown.ts
@@ -0,0 +1,11 @@
+/**
+ * They are both top types with means every value is compatible with them
+ * `any` is compatible with every sub-type except never. You can narrow the set of possible values by assigning a new type
+ * 
+ */
+
+const me: any = "Hello world"
+
+const myName: string = me
+
+
