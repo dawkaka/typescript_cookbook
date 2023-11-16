883b3a8fc02edf53f6ab2d55683cb90dfc06fd78
dawkaka <yousiph77@gmail.com>
2023-11-16 19:41:48 +0000

    bla

---
+++ index_signatures.ts
@@ -0,0 +1,26 @@
+const timings = {
+    "fettblog.eu": {
+        ttfb: 300,
+        fcp: 1000,
+        si: 1200,
+        lcp: 1500,
+        tti: 1100,
+        tbt: 10,
+    },
+    "typescript-book.com": {
+        ttfb: 400,
+        fcp: 1100,
+        si: 1100,
+        lcp: 2200,
+        tti: 1100,
+        tbt: 0,
+    },
+};
+
+
+
+/**
+ * Typo in book @26%
+ * Want: Metrics
+ * Got: Timings
+ */
---
+++ missing_props.ts
@@ -0,0 +1,9 @@
+type Settings = {
+    language: "en" | "de" | "fr";
+    theme?: "dracula" | "monokai" | "github";
+};
+
+
+function applySettings(settings: Settings) {
+    const theme = settings.theme
+}
