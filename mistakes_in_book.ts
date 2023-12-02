bd06063f5715cd1e9283fc8a47a992593b519265
dawkaka <yousiph77@gmail.com>
2023-12-02 13:31:57 +0000

    type in book

---
+++ mistakes_in_book.ts
@@ -0,0 +1,30 @@
+/**
+ * Typo in book @26%
+ * Want: Metrics
+ * Got: Timings
+ */
+
+
+/**
+ * Error in book @34%
+ * Note that we did remove the optional type modifier as well.
+ * Want: optional type modifier removed
+ * Got: optoinal type modifier not removed
+ */
+
+
+
+/**
+ * Typo @35%
+ * Want: JavaScript is a very flexible language
+ * Got:  JavaScript is a very flexible languages
+ * 
+ * Not the extra `s` at the end
+ */
+
+
+/**
+ * Error @35%
+ * Want:  We set the property, and since objects are passed by reference in JavaScript,
+ * Got:   We set the property, and since objects are passed by value in JavaScript, 
+ */
