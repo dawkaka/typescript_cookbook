c9d242ac84a0d4e3b42fbecf7a789f862d38bd5b
dawkaka <yousiph77@gmail.com>
2024-01-01 20:32:12 +0000

    using_thisType_to_define_this_in_objects

---
+++ src/using_thisType_to_define_this_in_objects.ts
@@ -0,0 +1,31 @@
+
+type FnObj = Record<string, () => any>
+
+type MapFnToProp<F extends FnObj> = {
+    [K in keyof F]: ReturnType<F[K]>
+}
+
+type Options<Data, Computed extends FnObj, Method> = {
+    data(this: {}): Data,
+    computed: Computed & ThisType<Data>
+    method: Method & ThisType<Data & MapFnToProp<Computed> & Method>
+}
+
+
+declare function create<Data, Computed extends FnObj, Method>(options: Options<Data, Computed, Method>): any
+
+create({
+    data() {
+        return { firstName: "Yussif", lastName: "Mohammed" }
+    },
+    computed: {
+        getName: function () {
+            return `${this.firstName} ${this.lastName}`
+        }
+    },
+    method: {
+        name: function () {
+            this.getName.toLocaleLowerCase()
+        }
+    }
+})
